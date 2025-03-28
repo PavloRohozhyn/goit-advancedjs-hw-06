8c425a215c183968aee6602c4afd8007f7b68198
Pavlo <pavlo.rohozhyn@gmail.com>
2025-03-28 14:46:38 +0100

    task 1

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,24 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age: number = 50;
+let name: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback = (a: number): number => {
+  return 100 + a;
+};
+
+// let callback2 = (a: number | string | boolean): number => {
+//   if (typeof a === "number") {
+//     return 100 + a;
+//   } else if (typeof a === "string") {
+//     return 100 + a.length;
+//   } else if (typeof a === "boolean") {
+//     return a ? 101 : 100;
+//   }
+//   // default value
+//   return 100;
+// };
 
 export {};
