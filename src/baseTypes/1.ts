90ee1e8172dbdbea41a9edeb0e878bf430f85bee
Pavlo <pavlo.rohozhyn@gmail.com>
2025-03-28 20:40:39 +0100

    task 1

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -9,16 +9,4 @@ let callback = (a: number): number => {
   return 100 + a;
 };
 
-// let callback2 = (a: number | string | boolean): number => {
-//   if (typeof a === "number") {
-//     return 100 + a;
-//   } else if (typeof a === "string") {
-//     return 100 + a.length;
-//   } else if (typeof a === "boolean") {
-//     return a ? 101 : 100;
-//   }
-//   // default value
-//   return 100;
-// };
-
 export {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,17 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum days {
+  mon = "Monday",
+  tues = "Tuesday",
+  wed = "Wednesday",
+  thurs = "Thursday",
+  fri = "Friday",
+  sat = "Saturday",
+  sun = "Sunday",
+}
+
+function isWeekend(day: days) {
+  return day === days.sat || day === days.sun;
+}
